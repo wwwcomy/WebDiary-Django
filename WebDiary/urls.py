6f5d50946a967190c39b028c5edf24a2fa483983
xingnliu <xingnliu@cisco.com>
2022-04-30 10:18:43 +0800

    Add the my_diary app, and add the first url

--- WebDiary/urls.py
+++ WebDiary/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import my_diary
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('diary/', include('my_diary.urls'))
 ]
